2dbd644535238c6243a2b3b59071c65e3bdcc163
M-Hidayatullah <hidayatullahblk@gmail.com>
2021-08-24 02:41:14 +0800

    config App Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+//import router
+import router from './router'
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+//import Bootstrap, Popper, jQuery
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+
+const app = createApp(App)
+
+//use vue router
+app.use(router)
+
+app.mount('#app')
